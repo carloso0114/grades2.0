922f430d45a93cf9928888dcffad495b40402a81
carloso0114 <carlossospinag@gmail.com>
2024-06-19 19:37:25 -0500

    update .env example and use env variables from to connect to my sql server

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('notas', 'root', 'admin123', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql'
 });
